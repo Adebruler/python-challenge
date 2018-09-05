7414cf323512fe3b7181e4aa56ab520cd901be63
A DeBruler <dudeeoman@gmail.com>
2018-09-05 15:56:17 -0400

    Completed PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,9 +20,24 @@ with open('election_data.csv', newline='') as csvfile:
         else:
             candict[cand] = [1,float(1)]
 
+most_votes=0
 for keys in candict:
     candict[keys][1] = candict[keys][0] / votes
+    if candict[keys][0] > most_votes:
+        most_votes=candict[keys][0]
+        winner=keys
 
-print(votes)
-print(candict)
-# print(candlist)
+# Print data into text file
+file=open('ElectionResults.txt',"w")
+file.write("Election Results")
+file.write('\n'+"-------------------------")
+file.write('\n'+f"Total Votes: {votes}")
+file.write('\n'+"-------------------------")
+for keys in candict:
+    file.write('\n'+f"{keys}: {candict[keys][1]*100:.3f}% ({candict[keys][0]})")
+file.write('\n'+"-------------------------")
+file.write('\n'+f"Winner: {winner}")
+file.write('\n'+"-------------------------")
+# Print Text File into Terminal
+file=open('ElectionResults.txt',"r")
+print(file.read())
